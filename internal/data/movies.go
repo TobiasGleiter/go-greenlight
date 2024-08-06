729331563b0c565a315c96020fae0821cf121958
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-08-06 18:45:27 +0200

    chore: sort

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 	"errors"
+	"fmt"
 	"time"
 
 	"github.com/TobiasGleiter/go-greenlight/internal/validator"
@@ -145,16 +146,16 @@ func (m MovieModel) Delete(id int64) error {
 	return nil
 }
 
-// Create a new GetAll() method which returns a slice of movies. Although we're not
-// using them right now, we've set this up to accept the various filter parameters as
-// arguments.
 func (m MovieModel) GetAll(title string, genres []string, filters Filters) ([]*Movie, error) {
-	query := `
-		SELECT id, created_at, title, year, runtime, genres, version
-		FROM movies
-		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') 
-		AND (genres @> $2 OR $2 = '{}')     
-		ORDER BY id`
+	// Add an ORDER BY clause and interpolate the sort column and direction. Importantly
+	// notice that we also include a secondary sort on the movie ID to ensure a
+	// consistent ordering.
+	query := fmt.Sprintf(`
+        SELECT id, created_at, title, year, runtime, genres, version
+        FROM movies
+        WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 = '') 
+        AND (genres @> $2 OR $2 = '{}')     
+        ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection())
 
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
