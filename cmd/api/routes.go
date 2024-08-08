2a1e4fd31c5e16fd82b679ddaf623a3293c445fe
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-08-08 19:06:19 +0200

    chore: users

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -19,13 +19,16 @@ func (app *application) routes() http.Handler {
 	// it as the custom error handler for 405 Method Not Allowed responses.
 	router.MethodNotAllowed = http.HandlerFunc(app.methodNotAllowedResponse)
 
+	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
+
 	// Add the route for the GET /v1/movies endpoint.
 	router.HandlerFunc(http.MethodGet, "/v1/movies", app.listMoviesHandler)
-	router.HandlerFunc(http.MethodGet, "/v1/healthcheck", app.healthcheckHandler)
 	router.HandlerFunc(http.MethodPost, "/v1/movies", app.createMovieHandler)
 	router.HandlerFunc(http.MethodGet, "/v1/movies/:id", app.showMovieHandler)
 	router.HandlerFunc(http.MethodPatch, "/v1/movies/:id", app.updateMovieHandler)
 	router.HandlerFunc(http.MethodDelete, "/v1/movies/:id", app.deleteMovieHandler)
+
+	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 
 	// Return the httprouter instance.
 	return app.recoverPanic(app.rateLimit(router))
