ee5fad86877f25aa23246a3160acb7fed0f7c34f
Tobias Gleiter <gleiter.tobias@outlook.de>
2024-08-17 17:17:47 +0200

    chore: vendoring

--- README.md
+++ README.md
@@ -9,3 +9,21 @@
 
 - `BODY='{"name": "Grace Smith", "email": "grace@example.com", "password": "invalid"}'`
 - `curl -d "$BODY" localhost:4000/v1/users`
+
+## Mirrors & Vendoring
+
+### Mirrors
+
+Where to look in the first place for the modules.
+
+- `export GOPROXY=https://goproxy.io,https://proxy.golang.org,direct`; primary mirror is goproxy.io, fallback is proxy.golang.org
+- `export GOPROXY=direct` disable module mirrors
+
+### Vendoring
+
+For complete ownership of the code.
+
+- `go mod vendor` (creates folder called vendor and stores all necessary modules)
+- `go clean -modcache` to check that the project runs without cached modules
+
+Downsides: checked in packages and performance of CI/CD systems that clone the repository
--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -4,7 +4,6 @@ import (
 	"errors"
 	"expvar"
 	"fmt"
-	"net"
 	"net/http"
 	"strconv"
 	"strings"
@@ -13,6 +12,8 @@ import (
 
 	"github.com/TobiasGleiter/go-greenlight/internal/data"
 	"github.com/TobiasGleiter/go-greenlight/internal/validator"
+
+	"github.com/tomasen/realip"
 	"golang.org/x/time/rate"
 )
 
@@ -47,6 +48,77 @@ func (app *application) recoverPanic(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+// func (app *application) rateLimit(next http.Handler) http.Handler {
+// 	// Define a client struct to hold the rate limiter and last seen time for each
+// 	// client.
+// 	type client struct {
+// 		limiter  *rate.Limiter
+// 		lastSeen time.Time
+// 	}
+
+// 	var (
+// 		mu sync.Mutex
+// 		// Update the map so the values are pointers to a client struct.
+// 		clients = make(map[string]*client)
+// 	)
+
+// 	// Launch a background goroutine which removes old entries from the clients map once
+// 	// every minute.
+// 	go func() {
+// 		for {
+// 			time.Sleep(time.Minute)
+
+// 			// Lock the mutex to prevent any rate limiter checks from happening while
+// 			// the cleanup is taking place.
+// 			mu.Lock()
+
+// 			// Loop through all clients. If they haven't been seen within the last three
+// 			// minutes, delete the corresponding entry from the map.
+// 			for ip, client := range clients {
+// 				if time.Since(client.lastSeen) > 3*time.Minute {
+// 					delete(clients, ip)
+// 				}
+// 			}
+
+// 			// Importantly, unlock the mutex when the cleanup is complete.
+// 			mu.Unlock()
+// 		}
+// 	}()
+
+// 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+// 		// Only carry out the check if rate limiting is enabled.
+// 		if app.config.limiter.enabled {
+// 			ip, _, err := net.SplitHostPort(r.RemoteAddr)
+// 			if err != nil {
+// 				app.serverErrorResponse(w, r, err)
+// 				return
+// 			}
+
+// 			mu.Lock()
+
+// 			if _, found := clients[ip]; !found {
+// 				clients[ip] = &client{
+// 					// Use the requests-per-second and burst values from the config
+// 					// struct.
+// 					limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst),
+// 				}
+// 			}
+
+// 			clients[ip].lastSeen = time.Now()
+
+// 			if !clients[ip].limiter.Allow() {
+// 				mu.Unlock()
+// 				app.rateLimitExceededResponse(w, r)
+// 				return
+// 			}
+
+// 			mu.Unlock()
+// 		}
+
+// 		next.ServeHTTP(w, r)
+// 	})
+// }
 
 func (app *application) rateLimit(next http.Handler) http.Handler {
 	// Define a client struct to hold the rate limiter and last seen time for each
@@ -86,20 +158,14 @@ func (app *application) rateLimit(next http.Handler) http.Handler {
 	}()
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Only carry out the check if rate limiting is enabled.
 		if app.config.limiter.enabled {
-			ip, _, err := net.SplitHostPort(r.RemoteAddr)
-			if err != nil {
-				app.serverErrorResponse(w, r, err)
-				return
-			}
+			// Use the realip.FromRequest() function to get the client's real IP address.
+			ip := realip.FromRequest(r)
 
 			mu.Lock()
 
 			if _, found := clients[ip]; !found {
 				clients[ip] = &client{
-					// Use the requests-per-second and burst values from the config
-					// struct.
 					limiter: rate.NewLimiter(rate.Limit(app.config.limiter.rps), app.config.limiter.burst),
 				}
 			}
